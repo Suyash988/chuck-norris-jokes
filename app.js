657c97552bf3bed2298ce4f1cca2787f10c1c1a0
SUYASH AGNIHOTRI <suyashagnihotri988@gmail.com>
2024-07-17 23:17:53 +0530

    fifth commit

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const loadJoke = async () => {
+    
+    const loader = document.getElementById('loader');
+    const jokeLoading = document.getElementById('loadingJoke');
+
+    loader.style.display = 'block';
+    jokeLoading.style.display = 'none';
 
     try {
         const chuckNorrisIsFetch = await fetch('https://api.chucknorris.io/jokes/random', {
@@ -10,8 +16,12 @@ const loadJoke = async () => {
         const joke = jokeData.value;
         
         const formattedJoke = `<p style = "font-size: 30px;line-height: 28px; color: #333; text-align: center;">${joke}</p>`;
-       
+      
+        loader.style.display = 'none';
+        jokeLoading.style.display = 'block';
         document.getElementById('loadingJoke').innerHTML  = formattedJoke;
+
+        
     } catch (error) {
        console.log(error) 
     }
